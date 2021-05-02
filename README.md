c4c8b629fb6168a3d6d8822814b64092313b9f25
Dan Crank <danno@danno.org>
2021-05-02 14:14:53 -0500

    still not sure I'm writing to RFM69

--- README.md
+++ README.md
@@ -1,8 +1,19 @@
-Prototype rover ground station code
+# Prototype rover ground station code
 
+## About
+
+This is part of a second iteration of a rover vehicle (the first
+being https://github.com/DanCrank/tank-bot-mk1). This repo will
+eventually grow into a ground station that can send commands to
+the rover, receive telemetry from the rover, and present some kind
+of user interface.
+
+The rover code can be found at https://github.com/DanCrank/rover.
+
+## Hardware references
 This code targets the following hardware...
 
 Raspberry Pi (any)
 
-Adafruit RFM69HCW Transceiver Radio Bonnet - 868 / 915 MHz
+Adafruit RFM69HCW Transceiver Radio Bonnet - 868 / 915 MHz<br>
 https://learn.adafruit.com/adafruit-radio-bonnets
--- src/main.rs
+++ src/main.rs
@@ -6,35 +6,48 @@
 *
 * Adafruit RFM69HCW Transceiver Radio Bonnet - 868 / 915 MHz
 * https://learn.adafruit.com/adafruit-radio-bonnets
+* https://cdn-shop.adafruit.com/product-files/3076/RFM69HCW-V1.1.pdf
+*
+* TODO: find the read timeout, and any other registers we need
+*   to set
+*
+* TODO: improve error handling with either error-chain or failure
+*     https://github.com/rust-lang-nursery/error-chain
+*     https://docs.rs/failure/0.1.8/failure/
 **************************************************************/
 
 use display_interface::DisplayError;
-//use linux_embedded_hal::spidev::{SpiModeFlags, SpidevOptions};
-//use linux_embedded_hal::sysfs_gpio::Direction;
-//use linux_embedded_hal::{Delay, Pin, Spidev};
-use rfm69::Rfm69;
-use rppal::gpio::{Gpio, OutputPin};
-use rppal::i2c::I2c;
-use rppal::spi::{Bus, Mode, /* Segment, */ SlaveSelect, Spi};
-use ssd1306::{
+use rfm69:: {
+    Rfm69,
+    registers:: { DataMode, InterPacketRxDelay, Modulation, ModulationShaping, ModulationType,
+                  PacketConfig, PacketDc, PacketFiltering, PacketFormat }
+};
+use rppal:: {
+    gpio::{Gpio, OutputPin},
+    i2c::I2c,
+    spi::{Bus, SlaveSelect, Spi}
+};
+use ssd1306:: {
     mode::TerminalMode,
     mode::terminal::TerminalModeError,
     prelude::*,
     Builder,
     I2CDIBuilder
 };
-use std::fmt::Write;
-use std::io::Error;
-use std::io::ErrorKind;
+use std:: {
+    fmt::Write,
+    io:: { Error, ErrorKind }
+};
 
 // this macro consumes a valueless Result that may contain an Error
 // we want to just print and ignore
-macro_rules! ignore_error {
+// TODO this is likely not the right way to do this
+macro_rules! dont_panic {
     ($a:expr) => {
         {
             match $a {
                 Ok(_) => (),
-                Err(e) => println!("{}", e),
+                Err(e) => println!("{:#?}", e),
             }
         }
     }
@@ -57,7 +70,9 @@ fn setup_display() -> Result<TerminalMode<I2CInterface<I2c>, DisplaySize128x32>,
         .connect(interface)
         .into();
     // TerminalModeError does not implement std::error::Error, so we have to handle its
-    // errors separately here
+    // errors separately in this abomination
+    // TODO this cannot possibly be the right way to do this - look at
+    //     https://doc.rust-lang.org/core/result/enum.Result.html#method.err
     match init_display(&mut disp) {
         Ok(_)=> Ok(disp),
         Err(e) =>
@@ -78,37 +93,68 @@ fn setup_display() -> Result<TerminalMode<I2CInterface<I2c>, DisplaySize128x32>,
 
 fn setup_radio() -> Result<Rfm69<OutputPin, Spi, linux_embedded_hal::Delay>, Box<dyn std::error::Error>> {
     // initialize the RFM69 radio
-    // see https://github.com/almusil/rfm69/blob/master/examples/send.rs
+    // see https://github.com/almusil/rfm69/blob/master/examples/receive.rs
     let gpio = Gpio::new()?;
     // Configure CS pin
     let mut cs = gpio.get(26)?.into_output();
     cs.set_high();
     // Configure SPI 8 bits, Mode 0
-    let spi = Spi::new(Bus::Spi0, SlaveSelect::Ss0, 1_000_000, Mode::Mode0)?;
+    let spi = Spi::new(Bus::Spi0, SlaveSelect::Ss0, 1_000_000, rppal::spi::Mode::Mode0)?;
     // Create rfm struct with defaults that are set after reset
-    Ok(Rfm69::new(spi, cs, linux_embedded_hal::Delay))
+    let mut rfm = Rfm69::new(spi, cs, linux_embedded_hal::Delay);
+    dont_panic!(rfm.modulation(Modulation { data_mode: DataMode::Packet,
+                                              modulation_type: ModulationType::Fsk,
+                                              shaping: ModulationShaping::Shaping00 }));
+    // TODO preamble
+    // TODO sync
+    dont_panic!(rfm.packet(PacketConfig { format: PacketFormat::Variable(0),
+                                            dc: PacketDc::None,
+                                            crc: true,
+                                            filtering: PacketFiltering::None, // TODO use Address
+                                            interpacket_rx_delay: InterPacketRxDelay::Delay1Bit, // ???
+                                            auto_rx_restart: true }));
+    // TODO node_address
+    // TODO aes
+    Result::Ok(rfm)
 }
 
 fn main() {
     let mut disp = setup_display().unwrap();
-    ignore_error!(disp.write_str("Rover Ground\nControl v0.0"));
+    dont_panic!(disp.write_str("Rover Ground\nControl v0.0"));
 
     let mut rfm = setup_radio().unwrap();
 
     // print content of all RFM registers
-    for (index, val) in rfm.read_all_regs().ok().unwrap().iter().enumerate() {
-        println!("Register 0x{:02x} = 0x{:02x}", index + 1, val);
+    match rfm.read_all_regs() {
+        Ok(regs) => {
+            for (index, val) in regs.iter().enumerate() {
+                println!("Register 0x{:02x} = 0x{:02x}", index + 1, val);
+            }
+        },
+        Err(e) => println!("Could not read RFM69 registers! {:#?}", e),
     }
 
     // prepare buffer to store the received data
     let mut buffer = [0; 64];
     // loop and receive packets
     loop {
-        // recv "blocks until there are any bytes available"
-        rfm.recv(&mut buffer).ok().unwrap();
-        // print received data
-        for (index, val) in buffer.iter().enumerate() {
-            println!("Value at {} = {}", index, val)
+        // recv claims it "blocks until there are any bytes available"
+        // but this is a lie; it actually has a hardcoded timeout of 100ms
+        // and returns a timeout error if there are no packets in that time.
+        match rfm.recv(&mut buffer) {
+            Ok(_) => {
+                // print received data
+                for (index, val) in buffer.iter().enumerate() {
+                    println!("Value at {} = {}", index, val)
+                }
+            },
+            Err(e) => {
+                match e {
+                    rfm69::Error::Timeout => (),
+                    _ => println!("Ow! {:#?}", e)
+                }
+                // thread::sleep(time::Duration::from_secs(1));
+            },
         }
     }
 }
